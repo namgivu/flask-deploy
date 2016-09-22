e4de3bbba7d1a711a92f892b33f57b0b446098b4
namgivu <namgivu@gmail.com>
2016-09-23 00:24:37 +0700

    daemon process info @deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,11 @@
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 appConf=hello.conf
+appDaemon=hello-app
+
+echo "Setup daemon process info..."
+sudo adduser $appDaemon --disabled-password -q
+echo "Setup daemon process info...DONE"
 
 echo "Un-register site..."
 sudo a2dissite $appConf
@@ -12,9 +17,9 @@ echo "Un-register site... DONE"
 echo "Register site..."
 sitesAvailable=/etc/apache2/sites-available
 sudo cp $SCRIPT_HOME/$appConf $sitesAvailable
-sudo a2ensite $appConf
+sudo a2ensite $appConf -q
 echo "Register site... DONE"
 
 echo "Reload apache..."
-sudo service apache2 reload
+sudo service apache2 --full-restart
 echo "Reload apache... DONE"
